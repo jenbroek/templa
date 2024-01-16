ada48f25570c922e4a6cb8fb361885559bca5719
Jens Broekens <jens.broekens@protonmail.com>
2024-01-16 01:58:46 +0100

    extract boilerplate test code into new method

--- export_test.go
+++ export_test.go
@@ -1,3 +1,24 @@
 package main
 
+import "testing"
+
 var MergeMaps = mergeMaps
+
+func RunTestCases[T any](t *testing.T, tcs map[string]T, fn func(t *testing.T, tc T)) {
+	for n, tc := range tcs {
+		// capture range variables
+		n, tc := n, tc
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+			fn(t, tc)
+		})
+	}
+}
+
+func ErrUnexpected(t *testing.T, err error) {
+	t.Errorf("unexpected err: %v", err)
+}
+
+func ErrNotEqual[T any](t *testing.T, got, want T) {
+	t.Errorf("got: %v, want %v", got, want)
+}
--- util_test.go
+++ util_test.go
@@ -4,121 +4,107 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/jensbrks/templa"
+	. "github.com/jensbrks/templa"
 )
 
 func TestMergeMaps(t *testing.T) {
-	testCases := []struct {
-		name string
+	type testCase struct {
 		dst  map[string]any
 		src  map[string]any
 		want map[string]any
 		fail bool
-	}{
-		{
-			name: "Adds new key",
-			dst:  map[string]any{"foo": "1"},
-			src:  map[string]any{"bar": "2"},
-			want: map[string]any{"foo": "1", "bar": "2"},
-			fail: false,
-		},
-		{
-			name: "Overwrites value of same non-map/slice type",
-			dst:  map[string]any{"foo": "1"},
-			src:  map[string]any{"foo": "2"},
-			want: map[string]any{"foo": "2"},
-			fail: false,
-		},
-		{
-			name: "Overwrites value of different non-map/slice type",
-			dst:  map[string]any{"foo": "1"},
-			src:  map[string]any{"foo": 1},
-			want: map[string]any{"foo": 1},
-			fail: false,
-		},
-		{
-			name: "Does nothing with nil dest map",
-			dst:  nil,
-			src:  map[string]any{"foo": "1"},
-			want: nil,
-			fail: false,
-		},
-		{
-			name: "Does nothing with nil source map",
-			dst:  map[string]any{"foo": "1"},
-			src:  nil,
-			want: map[string]any{"foo": "1"},
-			fail: false,
-		},
-		{
-			name: "Does nothing with empty source map",
-			dst:  map[string]any{"foo": "1"},
-			src:  map[string]any{},
-			want: map[string]any{"foo": "1"},
-			fail: false,
-		},
-		{
-			name: "Merges slice key",
-			dst:  map[string]any{"nums": []any{"1"}},
-			src:  map[string]any{"nums": []any{"2"}},
-			want: map[string]any{"nums": []any{"1", "2"}},
-			fail: false,
-		},
-		{
-			name: "Fails with non-slice and slice type",
-			dst:  map[string]any{"nums": []any{"1"}},
-			src:  map[string]any{"nums": "2"},
-			want: nil,
-			fail: true,
-		},
-		{
-			name: "Fails with different slice types",
-			dst:  map[string]any{"nums": []any{"1"}},
-			src:  map[string]any{"nums": []int{2}},
-			want: nil,
-			fail: true,
-		},
-		{
-			name: "Merges map key",
-			dst:  map[string]any{"nums": map[string]any{"1": "one"}},
-			src:  map[string]any{"nums": map[string]any{"2": "two"}},
-			want: map[string]any{"nums": map[string]any{"1": "one", "2": "two"}},
-			fail: false,
-		},
-		{
-			name: "Merges map key deeply",
-			dst:  map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one"}}},
-			src:  map[string]any{"nums": map[string]any{"1": map[string]any{"nl": "één"}}},
-			want: map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one", "nl": "één"}}},
-			fail: false,
-		},
-		{
-			name: "Fails with non-map and map type",
-			dst:  map[string]any{"nums": map[string]any{"1": "one"}},
-			src:  map[string]any{"nums": 2},
-			want: nil,
-			fail: true,
-		},
-		{
-			name: "Fails with different map types",
-			dst:  map[string]any{"nums": map[string]any{"1": "one"}},
-			src:  map[string]any{"nums": map[string]int{"2": 2}},
-			want: nil,
-			fail: true,
-		},
 	}
 
-	for _, tc := range testCases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
-			if err := main.MergeMaps(tc.dst, tc.src); err != nil {
+	RunTestCases(
+		t,
+		map[string]*testCase{
+			"Adds new key": &testCase{
+				dst:  map[string]any{"foo": "1"},
+				src:  map[string]any{"bar": "2"},
+				want: map[string]any{"foo": "1", "bar": "2"},
+				fail: false,
+			},
+			"Overwrites value of same non-map/slice type": &testCase{
+				dst:  map[string]any{"foo": "1"},
+				src:  map[string]any{"foo": "2"},
+				want: map[string]any{"foo": "2"},
+				fail: false,
+			},
+			"Overwrites value of different non-map/slice type": &testCase{
+				dst:  map[string]any{"foo": "1"},
+				src:  map[string]any{"foo": 1},
+				want: map[string]any{"foo": 1},
+				fail: false,
+			},
+			"Does nothing with nil dest map": &testCase{
+				dst:  nil,
+				src:  map[string]any{"foo": "1"},
+				want: nil,
+				fail: false,
+			},
+			"Does nothing with nil source map": &testCase{
+				dst:  map[string]any{"foo": "1"},
+				src:  nil,
+				want: map[string]any{"foo": "1"},
+				fail: false,
+			},
+			"Does nothing with empty source map": &testCase{
+				dst:  map[string]any{"foo": "1"},
+				src:  map[string]any{},
+				want: map[string]any{"foo": "1"},
+				fail: false,
+			},
+			"Merges slice key": &testCase{
+				dst:  map[string]any{"nums": []any{"1"}},
+				src:  map[string]any{"nums": []any{"2"}},
+				want: map[string]any{"nums": []any{"1", "2"}},
+				fail: false,
+			},
+			"Fails with non-slice and slice type": &testCase{
+				dst:  map[string]any{"nums": []any{"1"}},
+				src:  map[string]any{"nums": "2"},
+				want: nil,
+				fail: true,
+			},
+			"Fails with different slice types": &testCase{
+				dst:  map[string]any{"nums": []any{"1"}},
+				src:  map[string]any{"nums": []int{2}},
+				want: nil,
+				fail: true,
+			},
+			"Merges map key": &testCase{
+				dst:  map[string]any{"nums": map[string]any{"1": "one"}},
+				src:  map[string]any{"nums": map[string]any{"2": "two"}},
+				want: map[string]any{"nums": map[string]any{"1": "one", "2": "two"}},
+				fail: false,
+			},
+			"Merges map key deeply": &testCase{
+				dst:  map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one"}}},
+				src:  map[string]any{"nums": map[string]any{"1": map[string]any{"nl": "één"}}},
+				want: map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one", "nl": "één"}}},
+				fail: false,
+			},
+			"Fails with non-map and map type": &testCase{
+				dst:  map[string]any{"nums": map[string]any{"1": "one"}},
+				src:  map[string]any{"nums": 2},
+				want: nil,
+				fail: true,
+			},
+			"Fails with different map types": &testCase{
+				dst:  map[string]any{"nums": map[string]any{"1": "one"}},
+				src:  map[string]any{"nums": map[string]int{"2": 2}},
+				want: nil,
+				fail: true,
+			},
+		},
+		func(t *testing.T, tc *testCase) {
+			if err := MergeMaps(tc.dst, tc.src); err != nil {
 				if !tc.fail {
-					t.Errorf("unexpected err: %v", err)
+					ErrUnexpected(t, err)
 				}
 			} else if !reflect.DeepEqual(tc.dst, tc.want) {
-				t.Errorf("got: %v, want: %v", tc.dst, tc.want)
+				ErrNotEqual(t, tc.dst, tc.want)
 			}
-		})
-	}
+		},
+	)
 }
