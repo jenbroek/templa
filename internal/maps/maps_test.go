cc1c490c6e7255925e18f323ea7e367dfe3f37f2
Jens Broekens <jens.broekens@protonmail.com>
2024-01-21 08:55:52 +0100

    maps.Merge: rename test cases to be more clear

--- internal/maps/maps_test.go
+++ internal/maps/maps_test.go
@@ -30,24 +30,6 @@ func TestMerge(t *testing.T) {
 				want:  map[string]any{"foo": "1", "bar": "2"},
 				panic: false,
 			},
-			"Overwrites value of same type": &testCase{
-				dst:   map[string]any{"foo": "1"},
-				src:   map[string]any{"foo": "2"},
-				want:  map[string]any{"foo": "2"},
-				panic: false,
-			},
-			"Overwrites value of same pointer type": &testCase{
-				dst:   map[string]any{"foo": ptr(1)},
-				src:   map[string]any{"foo": ptr(2)},
-				want:  map[string]any{"foo": ptr(2)},
-				panic: false,
-			},
-			"Overwrites value of same array type": &testCase{
-				dst:   map[string]any{"foo": [1]int{1}},
-				src:   map[string]any{"foo": [1]int{2}},
-				want:  map[string]any{"foo": [1]int{2}},
-				panic: false,
-			},
 			"Does nothing with nil dest map": &testCase{
 				dst:   nil,
 				src:   map[string]any{"foo": "1"},
@@ -66,28 +48,16 @@ func TestMerge(t *testing.T) {
 				want:  map[string]any{"foo": "1"},
 				panic: false,
 			},
-			"Merges array into assignable slice": &testCase{
-				dst:   map[string]any{"nums": []any{"1"}},
-				src:   map[string]any{"nums": [1]int{2}},
-				want:  map[string]any{"nums": []any{"1", 2}},
+			"Overwrites value if same primitive type": &testCase{
+				dst:   map[string]any{"foo": "1"},
+				src:   map[string]any{"foo": "2"},
+				want:  map[string]any{"foo": "2"},
 				panic: false,
 			},
-			"Panics with non-assignable array types": &testCase{
-				dst:   map[string]any{"nums": [1]string{"1"}},
-				src:   map[string]any{"nums": [1]int{1}},
-				want:  nil,
-				panic: true,
-			},
-			"Merges assignable slice key": &testCase{
-				dst:   map[string]any{"nums": []any{"1"}},
-				src:   map[string]any{"nums": []int{2}},
-				want:  map[string]any{"nums": []any{"1", 2}},
-				panic: false,
-			},
-			"Merges assignable pointer slice key": &testCase{
-				dst:   map[string]any{"nums": &[]any{"1"}},
-				src:   map[string]any{"nums": &[]int{2}},
-				want:  map[string]any{"nums": &[]any{"1", 2}},
+			"Overwrites value if same primitive pointer type": &testCase{
+				dst:   map[string]any{"foo": ptr(1)},
+				src:   map[string]any{"foo": ptr(2)},
+				want:  map[string]any{"foo": ptr(2)},
 				panic: false,
 			},
 			"Panics with non-assignable types": &testCase{
@@ -96,19 +66,49 @@ func TestMerge(t *testing.T) {
 				want:  nil,
 				panic: true,
 			},
+			"Overwrites value if same array type": &testCase{
+				dst:   map[string]any{"foo": [1]int{1}},
+				src:   map[string]any{"foo": [1]int{2}},
+				want:  map[string]any{"foo": [1]int{2}},
+				panic: false,
+			},
+			"Merges array into slice if assignable": &testCase{
+				dst:   map[string]any{"nums": []any{"1"}},
+				src:   map[string]any{"nums": [1]int{2}},
+				want:  map[string]any{"nums": []any{"1", 2}},
+				panic: false,
+			},
+			"Panics with different array types": &testCase{
+				dst:   map[string]any{"nums": [1]string{"1"}},
+				src:   map[string]any{"nums": [1]int{1}},
+				want:  nil,
+				panic: true,
+			},
+			"Merges slices if assignable": &testCase{
+				dst:   map[string]any{"nums": []any{"1"}},
+				src:   map[string]any{"nums": []int{2}},
+				want:  map[string]any{"nums": []any{"1", 2}},
+				panic: false,
+			},
+			"Merges slices if assignable (pointer)": &testCase{
+				dst:   map[string]any{"nums": &[]any{"1"}},
+				src:   map[string]any{"nums": &[]int{2}},
+				want:  map[string]any{"nums": &[]any{"1", 2}},
+				panic: false,
+			},
 			"Panics with non-assignable slice types": &testCase{
 				dst:   map[string]any{"nums": []string{"1"}},
 				src:   map[string]any{"nums": []int{2}},
 				want:  nil,
 				panic: true,
 			},
-			"Merges assignable map key": &testCase{
+			"Merges maps if assignable": &testCase{
 				dst:   map[string]any{"nums": map[string]any{"1": "one"}},
 				src:   map[string]any{"nums": map[string]int{"2": 2}},
 				want:  map[string]any{"nums": map[string]any{"1": "one", "2": 2}},
 				panic: false,
 			},
-			"Merges map key deeply": &testCase{
+			"Merges maps deeply if assignable": &testCase{
 				dst:   map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one"}}},
 				src:   map[string]any{"nums": map[string]any{"1": map[string]any{"nl": "één"}}},
 				want:  map[string]any{"nums": map[string]any{"1": map[string]any{"en": "one", "nl": "één"}}},
